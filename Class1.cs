16b6aa8c046eea2ec430f8f01af6a2eccd9845d5
Sharky <100982532+SharkyOnGitHub@users.noreply.github.com>
2024-10-28 21:32:55 -0700

    Source code

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace CrashOnLaunch
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class CrashMod : MonoBehaviour
+    {
+        void Start()
+        {
+            
+            throw new Exception("Crashing client..");
+        }
+    }
+}
